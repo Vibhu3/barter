68d2912f3a479e7c8c0e2181381ecc60a6c6110e
Vibhu3 <65866042+Vibhu3@users.noreply.github.com>
2021-01-30 22:29:34 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import SignUpLoginScreen from './screens/SignUpLoginScreen';
-
-export default class App extends React.Component {
-  render(){
-    return(
-      <SignUpLoginScreen />
-    )
-  }
-}
+import WelcomeScreen from './screens/WelcomeScreen'
+import styles from './Styles'
+import MyTextInput from './components/MyTextInput'
+export default function App() {
+  return (
+    
+     <WelcomeScreen/>
